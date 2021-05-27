ed96df606d0aa7d9c41ff6497b8081f6b04bbbe3
kajikentaro <k.kentarou0412@gmail.com>
2021-05-28 02:48:07 +0900

    first commit

---
+++ Overlay PictureBox Demo/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Overlay_PictureBox_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Overlay PictureBox Demo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Overlay_PictureBox_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //重ねて表示するために両方の位置を(0,0)に設定
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox2.Location = new Point(0, 0);
+
+            //大きさを(500,500)に設定
+            pictureBox1.ClientSize = new Size(500, 500);
+            pictureBox2.ClientSize = new Size(500, 500);
+
+            //pictureBox2の親要素をpictureBox1に設定する
+            pictureBox2.Parent = pictureBox1;
+
+            //pictureBox1に色を塗る(わかりやすくするため)
+            pictureBox1.BackColor = Color.DeepSkyBlue;
+
+            //pictureBox2を透過させる
+            pictureBox2.BackColor = Color.Transparent;
+        }
+        Bitmap overlay = new Bitmap(500, 500);
+        private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics graphics = Graphics.FromImage(overlay);
+
+            //透明色で塗りつぶす
+            graphics.Clear(Color.Transparent);
+
+            //四角形の描写
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            graphics.FillRectangle(blueBrush, e.X, e.Y, 70, 70);
+
+            //graphicsの開放
+            graphics.Dispose();
+
+            pictureBox2.Image = overlay;
+        }
+    }
+}
